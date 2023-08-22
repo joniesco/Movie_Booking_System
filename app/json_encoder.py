eba512a004e3ef07c315333cab6edd54afdb4b71
jonathan escojido <73218886+joniesco@users.noreply.github.com>
2023-08-22 17:29:04 +0300

    Delete json_encoder.py

--- app/json_encoder.py
+++
@@ -1,8 +0,0 @@
-import json
-from bson import ObjectId
-
-class CustomJSONEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, ObjectId):
-            return str(obj)
-        return super().default(obj)
